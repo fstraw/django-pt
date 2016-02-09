60d7d56f6d456aaf2664193ffa3b132a37f27814
Brandon <fstraw@lowestfrequency.com>
2016-02-09 15:02:41 -0500

    Added FT for add pit page

--- functional_tests.py
+++ functional_tests.py
@@ -14,14 +14,20 @@ class NewVisitorTest(unittest.TestCase):
 	## She goes to check out the homepage
 		self.browser.get('http://localhost:8000') 
 
-##She sees that it is called 'EPEI Project Tracking'
+		##She sees that it is called 'EPEI Project Tracking'
 		self.assertIn('EPEI Project Tracking', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('EPEI Project Tracking', header_text)
 
-##She sees an overview of current projects, with the most pressing listed at the top
-
-##She wants to find one of her projects in the database in the omnibox
+		##She sees an overview of current projects, with the most pressing listed at the top
+		
+		##She wants to create a new project. She sees a button called 'Add Project'
+		add_button = self.browser.find_element_by_id('id_add')
+		##She clicks the button to add a project
+		add_button.click()
+		##See sees that it is called "Add Project"
+		self.assertIn('Add Project', self.browser.title)
+		##She wants to find one of her projects in the database in the omnibox
 		inputbox = self.browser.find_element_by_id('id_search')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
@@ -38,8 +44,6 @@ class NewVisitorTest(unittest.TestCase):
 			"New project did not appear in table"
 		)
 		self.fail('Finish the test!')
-
-##She wants to create a new project
 
 ##She moves on to something else
 if __name__ == '__main__':
