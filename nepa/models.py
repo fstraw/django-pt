39a9ce44aa31134da298a9bfc5eb6d6efac9d550
Brandon <fstraw@lowestfrequency.com>
2016-02-09 19:03:30 -0500

    Added Project model and associated UT

--- nepa/models.py
+++ nepa/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+## EP Project
+class Project(models.Model):
+	epid = models.TextField(default='')
+## Related Projects and previous designations
+## EP Manager
+## PI Number
+## GDOT Project Number
+## Scope of Services
+## NEPA
+	## Document Due Date
+## Air
+	## Document Due Date
+## Noise
+	## Document Due Date
+## Ecology
+	##Document Due Date
+## Archaeology
+	##Document Due Date
+## History
+	##Document Due Date
--- nepa/tests.py
+++ nepa/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from nepa.views import home_page, add_page
+from nepa.models import Project
 
 
 class HomePageTest(TestCase): 
@@ -20,3 +21,19 @@ class NewProjectTest(TestCase):
 	def test_url_resolves_to_add_project_view(self):
 		found = resolve('/add/')
 		self.assertEqual(found.func, add_page)
+	def test_add_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = add_page(request)
+		expected_html = render_to_string('add.html')
+		self.assertEqual(response.content.decode(), expected_html)
+
+class ModelTests(TestCase):
+	def test_saving_and_getting_projects(self):
+		epid = 'PBQ1601'
+		first_project = Project()
+		first_project.epid = epid
+		first_project.save()
+		
+		saved_objects = Project.objects.all()
+		first_saved_project = saved_objects[0]
+		self.assertEqual(first_saved_project.epid, epid)
