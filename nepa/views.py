b379731587f9854987fc7dcb12342eeaa2d140b7
Brandon <fstraw@lowestfrequency.com>
2016-05-03 16:40:12 -0400

    Refactored helper functions

--- nepa/views.py
+++ nepa/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.template import RequestContext, loader
 from django.core.exceptions import ObjectDoesNotExist
+
 from nepa.forms import ProjectForm, NepaForm, AirForm, NoiseForm, EcologyForm, AquaticsForm, ArchaeologyForm, HistoryForm
 from nepa.models import Project, Nepa, ProjectNumbers, PINumbers
+from nepa.utils import initial_form_lookup, form_lookup, blank_form_lookup
 
 @login_required
 def home_page(request):	
@@ -210,98 +212,6 @@ def nepa_add(request, projectid):
 	else:
 		form = NepaForm(initial={'project':project})	
 		return render(request, 'add_document.html', {'form':form, 'project':project})
-
-def form_lookup(request, form_from_url, instance=None):
-	form_dict = {
-	'airform': AirForm(request.POST),
-	'noiseform': NoiseForm(request.POST),
-	'archform': ArchaeologyForm(request.POST),
-	'ecoform': EcologyForm(request.POST),
-	'aquaform': AquaticsForm(request.POST),
-	'histform': HistoryForm(request.POST),
-	}
-	##with instance
-	inst_dict = {
-	'airform': AirForm(request.POST, instance=instance),
-	'noiseform': NoiseForm(request.POST, instance=instance),
-	'archform': ArchaeologyForm(request.POST, instance=instance),
-	'ecoform': EcologyForm(request.POST, instance=instance),
-	'aquaform': AquaticsForm(request.POST, instance=instance),
-	'histform': HistoryForm(request.POST, instance=instance),
-	}
-	if instance:
-		return inst_dict[form_from_url]
-	return form_dict[form_from_url]
-
-def blank_form_lookup(ss_type, special_study):
-	try:
-		air = AirForm(instance=special_study)
-	except:
-		air = ''
-	try:
-		noise = NoiseForm(instance=special_study)
-	except:
-		noise = ''
-	try:
-		ecology = EcologyForm(instance=special_study)
-	except:
-		ecology = ''
-	try:
-		aquatics = AquaticsForm(instance=special_study)
-	except:
-		aquatics = ''
-	try:
-		archaeology = ArchaeologyForm(instance=special_study)
-	except:
-		archaeology = ''
-	try:
-		history = HistoryForm(instance=special_study)
-	except:
-		history = ''
-	form_dict = {
-		'air' : air,
-		'noise' : noise,
-		'ecology': ecology,
-		'aquatics': aquatics,
-		'archaeology': archaeology,
-		'history': history,
-	}
-	return form_dict[ss_type]
-
-def initial_form_lookup(ss_type, project_from_db):
-	try:
-		air = AirForm(initial={'project': project_from_db})
-	except:
-		air = ''
-	try:
-		noise = NoiseForm(initial={'project' : project_from_db})
-	except:
-		noise = ''
-	try:
-		ecology = EcologyForm(initial={'project' : project_from_db})
-	except:
-		ecology = ''
-	try:
-		aquatics = AquaticsForm(initial={'project' : project_from_db})
-	except:
-		aquatics = ''
-	try:
-		archaeology = ArchaeologyForm(initial={'project' : project_from_db})
-	except:
-		archaeology = ''
-	try:
-		history = HistoryForm(initial={'project' : project_from_db})
-	except:
-		history = ''
-	form_dict = {
-		'air' : air,
-		'noise' : noise,
-		'ecology': ecology,
-		'aquatics': aquatics,
-		'archaeology': archaeology,
-		'history': history,
-	}
-	return form_dict[ss_type]
 
 @login_required
 def ss_add(request, projectid, ss_type, form_type):
