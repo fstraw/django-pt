18d7e89e957be5401306b1be28f60d9af64b9882
Brandon <fstraw@lowestfrequency.com>
2016-05-06 13:34:17 -0400

    Updated settings for new deployment server

--- eptrack/settings.py
+++ eptrack/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.postgresql_psycopg2',
        'NAME': 'django',
        'USER': 'django',
-       'PASSWORD': 'GqA2n8lmMa',
+       'PASSWORD': 'h90xc9039a',
        'HOST': 'localhost',
        'PORT': '',
        'TEST': {
