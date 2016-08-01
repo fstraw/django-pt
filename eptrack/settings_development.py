11178e5fbf3c925c43370b8f5140c4542ae4e8fd
Brandon <fstraw@lowestfrequency.com>
2016-08-01 11:01:41 -0400

    Modified settings for deployment

--- eptrack/settings_development.py
+++ eptrack/settings_development.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from key import SECRET_KEY
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '19o@zrf639gd8s#lccd&5_ccuf9tnye+&izua$g*87#9l$c0cj'
+# SECRET_KEY = '19o@zrf639gd8s#lccd&5_ccuf9tnye+&izua$g*87#9l$c0cj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
